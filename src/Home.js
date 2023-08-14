3d1b58df89e35a95ec72cf68fa126e6854958d4c
Ash1R <ashir.rao26@gmail.com>
2023-08-13 17:25:51 -0700

    working towards the end

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,29 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import {
   Container,
   Grid,
-  Card,
   Input,
   Dropdown,
   Header,
   Divider,
   Image,
+  Button,
+  Card,
 } from "semantic-ui-react";
 import HamburgerMenu from "./hamenu";
-
+import {
+  getFirestore,
+  setDoc,
+  doc,
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+} from "firebase/firestore";
 const App = () => {
   const [value, setValue] = useState("United States");
+  const [trigger, setTrigger] = useState(true);
+
   const handleClick = (e, { value }) => {
     setValue(value);
   };
@@ -29,104 +41,267 @@ const App = () => {
   map.set("nz", "New Zealand (beta)");
   map.set("in", "India (beta, English)");
 
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Month is zero-based
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
+
   const d = new Date();
-  const [date, setDate] = useState(d.toString());
-  const newsData = [
-    {
-      title: "Breaking News",
-      image: "",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et rutrum mauris, et tincidunt est.",
-      id: "1",
-    },
-    {
-      title: "Latest Update",
-      image: "",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut neque condimentum, ullamcorper mi eu, commodo felis.",
-      id: "2",
-    },
-    {
-      title: "Breaking News",
-      image: "",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et rutrum mauris, et tincidunt est.",
-      id: "1",
-    },
-    {
-      title: "Latest Update",
-      image: "",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut neque condimentum, ullamcorper mi eu, commodo felis.",
-      id: "2",
-    },
-    {
-      title: "Breaking News",
-      image: "",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et rutrum mauris, et tincidunt est.",
-      id: "1",
-    },
-    {
-      title: "Latest Update",
-      image: "",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut neque condimentum, ullamcorper mi eu, commodo felis.",
-      id: "2",
-    },
-    // Add more news items here...
-  ];
+  const [date, setDate] = useState(formatDate(d));
+  const [newsData, setNewsData] = useState([]);
+
+  const [searchString, setSearchString] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const truncateDescription = (description, maxLength) => {
+    console.log(description);
+    if (description == null) {
+      return "";
+    }
+    if (description.length <= maxLength) {
+      return description;
+    }
+    return description.slice(0, maxLength) + "...";
+  };
+
+  const handleSearch_ = async (dat, val) => {
+    function isNumeric_(str) {
+      if (typeof str != "string") return false; // we only process strings!
+      return (
+        !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str))
+      ); // ...and ensure strings of whitespace fail
+    }
+    function processObject_(inputObject) {
+      let highestValue = -Infinity;
+      let chosenKey = null;
+
+      for (const key in inputObject) {
+        if (inputObject.hasOwnProperty(key)) {
+          const value = inputObject[key];
+
+          if (
+            isNumeric_(key) &&
+            Number(key) <= 1 &&
+            Number(key) !== Infinity &&
+            Number(key) > highestValue
+          ) {
+            highestValue = value;
+            chosenKey = key;
+          }
+        }
+      }
+
+      if (chosenKey === null) {
+        return null;
+      }
+
+      const newObject = {
+        cat: inputObject.cat,
+        date: inputObject.date,
+        id: inputObject.id,
+        keyword: inputObject.keyword,
+      };
+
+      const returnObj = { ...newObject, ...inputObject[chosenKey] };
+
+      return returnObj;
+    }
+
+    try {
+      const db = getFirestore();
+      // Create a query to filter documents based on whether the search string is contained in the name field
+      const q = query(collection(db, "topics"), where("date", ">=", dat));
+
+      // Execute the query
+      const querySnapshot = await getDocs(q);
+
+      // Process the data and update the search results
+      const results = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      var returnVal = [];
+      for (const result in results) {
+        returnVal.push(processObject_(results[result]));
+      }
+
+      const groupedArticles = {
+        general: [],
+        business: [],
+        science: [],
+        health: [],
+        sports: [],
+        technology: [],
+        entertainment: [],
+      };
+
+      returnVal.forEach((article) => {
+        const cate = article.cat;
+        if (!groupedArticles[cate]) {
+          groupedArticles[cate] = [];
+        }
+        groupedArticles[cate].push(article);
+      });
+
+      console.log(groupedArticles);
+
+      await setNewsData(groupedArticles);
+    } catch (error) {
+      setSearchResults([]);
+    }
+  };
+  useEffect(async () => {
+    const handleSearch = async (dat, val) => {
+      const isNumeric = async (str) => {
+        if (typeof str != "string") return false; // we only process strings!
+        return (
+          !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+          !isNaN(parseFloat(str))
+        ); // ...and ensure strings of whitespace fail
+      };
+      const processObject = async (inputObject) => {
+        let highestValue = -Infinity;
+        let chosenKey = null;
+
+        for (const key in inputObject) {
+          if (inputObject.hasOwnProperty(key)) {
+            const value = inputObject[key];
+
+            if (
+              isNumeric(key) &&
+              Number(key) <= 1 &&
+              Number(key) !== Infinity &&
+              Number(key) > highestValue
+            ) {
+              highestValue = value;
+              chosenKey = key;
+            }
+          }
+        }
+
+        if (chosenKey === null) {
+          return null;
+        }
+
+        const newObject = {
+          cat: inputObject.cat,
+          date: inputObject.date,
+          id: inputObject.id,
+          keyword: inputObject.keyword,
+        };
+
+        const returnObj = { ...newObject, ...inputObject[chosenKey] };
+
+        return returnObj;
+      };
+
+      try {
+        const db = getFirestore();
+        // Create a query to filter documents based on whether the search string is contained in the name field
+        const q = query(collection(db, "topics"), where("date", ">=", dat));
+
+        // Execute the query
+        const querySnapshot = await getDocs(q);
+
+        // Process the data and update the search results
+        const results = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(results);
+        var returnVal = [];
+        for (const result in results) {
+          var inputObject = results[result];
+          let highestValue = -Infinity;
+          let chosenKey = null;
+
+          for (const key in inputObject) {
+            if (inputObject.hasOwnProperty(key)) {
+              const value = inputObject[key];
+
+              if (
+                isNumeric(key) &&
+                Number(key) <= 1 &&
+                Number(key) !== Infinity &&
+                Number(key) > highestValue
+              ) {
+                highestValue = value;
+                chosenKey = key;
+              }
+            }
+          }
+
+          const newObject = {
+            cat: inputObject.cat,
+            date: inputObject.date,
+            id: inputObject.id,
+            keyword: inputObject.keyword,
+          };
+
+          const returnObj = { ...newObject, ...inputObject[chosenKey] };
+          returnVal.push(returnObj);
+        }
+
+        console.log(returnVal);
+
+        const groupedArticles = {};
+
+        returnVal.forEach((article) => {
+          const cate = article.cat;
+          if (!groupedArticles[cate]) {
+            groupedArticles[cate] = [];
+          }
+          groupedArticles[cate].push(article);
+        });
+
+        console.log(groupedArticles);
+
+        setNewsData(groupedArticles);
+      } catch (error) {
+        setSearchResults([]);
+      }
+    };
+    handleSearch(date, value);
+  }, []);
 
   return (
     <Container>
       <Header style={{ textAlign: "center", marginTop: "5px" }} size="huge">
         Perspectives
       </Header>
-      <HamburgerMenu date={date}></HamburgerMenu>
+      <HamburgerMenu></HamburgerMenu>
 
       <Divider></Divider>
       <Grid columns={3} stackable>
         <Grid.Column computer={12}>
-          <Header as="h2">Politics</Header>
-
-          {newsData
-            .filter((news, index) => index % 2 === 0)
-            .map((news, index) => (
-              <Container>
-                <Grid.Row
-                  style={{ display: "flex", justifyContent: "space-between" }}
-                  computer={6}
-                  key={index}
-                >
-                  <Link to={"/analysis?id=" + newsData[index].id}>
-                    <Card>
-                      <Image src={newsData[index].image} wrapped ui={false} />
-                      <Card.Content>
-                        <Card.Header>{newsData[index].title}</Card.Header>
-                        <Card.Description>
-                          {newsData[index].description}
-                        </Card.Description>
-                      </Card.Content>
-                    </Card>
-                  </Link>
-                  <Link to={"/analysis?id=" + newsData[index + 1].id}>
-                    <Card>
-                      <Image
-                        src={newsData[index + 1].image}
-                        wrapped
-                        ui={false}
-                      />
-                      <Card.Content>
-                        <Card.Header>{newsData[index + 1].title}</Card.Header>
-                        <Card.Description>
-                          {newsData[index + 1].description}
-                        </Card.Description>
-                      </Card.Content>
-                    </Card>
-                  </Link>
-                </Grid.Row>
-              </Container>
-            ))}
+          {Object.entries(newsData).map(([category, articles]) => (
+            <div key={category}>
+              <Header as="h2">{category}</Header>
+              <Grid columns={2} stackable doubling>
+                {articles.map((article) => (
+                  <Grid.Column key={article.title}>
+                    <Link to={"/analysis?id=" + article.id}>
+                      <Card fluid>
+                        <Image src={article.urlToImage} alt={article.title} />
+                        <Card.Content>
+                          <Card.Header>{article.title}</Card.Header>
+                          <Card.Meta>{article.author}</Card.Meta>
+                          <Card.Description>
+                            {truncateDescription(article.description, 50)}
+                          </Card.Description>
+                        </Card.Content>
+                      </Card>
+                    </Link>
+                  </Grid.Column>
+                ))}
+              </Grid>
+              <Divider />
+            </div>
+          ))}
         </Grid.Column>
 
         <Grid.Column computer={4}>
@@ -152,6 +327,8 @@ const App = () => {
             value={date}
             label="date"
           ></Input>
+          <Divider hidden></Divider>
+          <Button onClick={() => handleSearch_(date, value)}>reload</Button>
         </Grid.Column>
       </Grid>
     </Container>
--- src/analysis.js
+++ src/analysis.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 import {
   Container,
   Grid,
@@ -10,32 +12,107 @@ import {
   Image,
   Table,
   Menu,
+  Segment,
 } from "semantic-ui-react";
+import {
+  getFirestore,
+  setDoc,
+  getDoc,
+  doc,
+  collection,
+  query,
+  where,
+  getDocs,
+  orderBy,
+} from "firebase/firestore";
 import HamburgerMenu from "./hamenu";
+import { initializeApp } from "firebase/app";
 
 const Analysis = () => {
-  const queryparam = new URLSearchParams(window.location.search);
-  const tableData = [
-    {
-      category: "Unopinionated",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    {
-      category: "Unopinionated",
-      content:
-        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    },
-    {
-      category: "Mildly Opinionated",
-      content:
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
-    },
-    {
-      category: "Opinionated",
-      content:
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.",
-    },
-  ];
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const id = queryParams.get("id");
+
+  const [data, setData] = useState(null);
+  const [categorizedData, setCategorizedData] = useState([]);
+
+  const truncateDescription = (description, maxLength) => {
+    console.log(description);
+    if (description == null) {
+      return "";
+    }
+    if (description.length <= maxLength) {
+      return description;
+    }
+    return description.slice(0, maxLength) + "...";
+  };
+
+  useEffect(() => {
+    const db = getFirestore();
+
+    const fetchData = async () => {
+      try {
+        const docRef = doc(db, "topics", id);
+        const docSnapshot = await getDoc(docRef);
+
+        if (docSnapshot.exists()) {
+          setData(docSnapshot.data());
+        } else {
+          console.log("Document does not exist");
+        }
+      } catch (error) {
+        console.error("Error fetching document:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  useEffect(() => {
+    if (data) {
+      const categorized = [];
+
+      for (const key in data) {
+        if (
+          data.hasOwnProperty(key) &&
+          key !== "cat" &&
+          key !== "country" &&
+          key !== "date" &&
+          key !== "keyword"
+        ) {
+          const numericalValue = parseFloat(key);
+
+          let category = "Unable to categorize";
+
+          if (!isNaN(numericalValue)) {
+            if (numericalValue >= 0.9 && numericalValue <= 0.97) {
+              category = "Unopinionated";
+            } else if (numericalValue >= 0.8 && numericalValue < 0.9) {
+              category = "Mildly Opinionated";
+            } else if (numericalValue < 0.8) {
+              category = "Opinionated";
+            }
+          }
+
+          categorized.push({ key, value: data[key], category });
+        }
+      }
+      console.log(categorized);
+      const categorizedNews = {
+        Unopinionated: [],
+        "Mildly Opinionated": [],
+        Opinionated: [],
+        "Unable to categorize": [],
+      };
+      categorized.forEach((newsItem) => {
+        const { category } = newsItem;
+        categorizedNews[category].push(newsItem);
+      });
+      console.log(categorizedNews);
+      console.log(categorizedNews["Unopinionated"]);
+      setCategorizedData(categorizedNews);
+    }
+  }, [data]);
 
   return (
     <Container>
@@ -60,32 +137,70 @@ const Analysis = () => {
       <Header style={{ margin: "auto" }} as="h2">
         More Perspectives
       </Header>
-
-      <Table collapsing>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Unopinionated</Table.HeaderCell>
-            <Table.HeaderCell>Mildly Opinionated</Table.HeaderCell>
-            <Table.HeaderCell>Opinionated</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {tableData.map((row, index) => (
-            <Table.Row key={index}>
-              <Table.Cell>
-                {row.category === "Unopinionated" ? row.content : ""}
-              </Table.Cell>
-              <Table.Cell>
-                {row.category === "Mildly Opinionated" ? row.content : ""}
-              </Table.Cell>
-              <Table.Cell>
-                {row.category === "Opinionated" ? row.content : ""}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+      <Divider hidden></Divider>
+      <Divider hidden></Divider>
+      <Grid columns={3} stackable>
+        <Grid.Column>
+          <Segment>
+            <Header>Opinionated</Header>
+            {categorizedData["Opinionated"] ? (
+              categorizedData["Opinionated"].map((newsItem, index) => (
+                <Card key={index} style={{ margin: "auto" }}>
+                  <Image src={newsItem["value"]["urlToImage"]} />
+                  <Card.Content>
+                    <Card.Header>{newsItem["value"]["title"]}</Card.Header>
+                    <Card.Description>
+                      {truncateDescription(newsItem["value"]["description"])}
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+              ))
+            ) : (
+              <Loader active>Loading...</Loader>
+            )}
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>
+            <Header>Mildly Opinionated</Header>
+            {categorizedData["Mildly Opinionated"] ? (
+              categorizedData["Mildly Opinionated"].map((newsItem, index) => (
+                <Card key={index} style={{ margin: "auto" }}>
+                  <Image src={newsItem["value"]["urlToImage"]} />
+                  <Card.Content>
+                    <Card.Header>{newsItem["value"]["title"]}</Card.Header>
+                    <Card.Description>
+                      {truncateDescription(newsItem["value"]["description"])}
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+              ))
+            ) : (
+              <Loader active>Loading...</Loader>
+            )}
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>
+            <Header>Unopinionated</Header>
+            {categorizedData["Unopinionated"] ? (
+              categorizedData["Unopinionated"].map((newsItem, index) => (
+                <Card key={index} style={{ margin: "auto" }}>
+                  <Image src={newsItem["value"]["urlToImage"]} />
+                  <Card.Content>
+                    <Card.Header>{newsItem["value"]["title"]}</Card.Header>
+                    <Card.Description>
+                      {truncateDescription(newsItem["value"]["description"])}
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+              ))
+            ) : (
+              <Loader active>Loading...</Loader>
+            )}
+          </Segment>
+        </Grid.Column>
+      </Grid>
     </Container>
   );
 };
